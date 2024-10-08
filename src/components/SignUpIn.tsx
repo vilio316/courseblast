1662b8dac6ae4edfd5b0f28bf3630eb0593d3c34
David <atembarry12@gmail.com>
2024-10-08 16:17:22 +0100

    Form Designed, needs Illustrations

--- src/components/SignUpIn.tsx
+++ src/components/SignUpIn.tsx
@@ -1,34 +1,69 @@
 //import { useState } from "react"
 
+import { FaApple, FaFacebook, FaGoogle } from "react-icons/fa";
+
 export function SignUp(){
    // let [password_show, changeState] = useState(false)
 
 
     return(
         <>
-        <div className="grid place-items-center p-2 h-screen w-11/12 mx-auto">
-                <div className="w-11/12 h-5/6 shadow-md shadow-black rounded-xl p-4 m-4">
+        <div className="grid place-items-center p-2 w-11/12 mx-auto">
+                <div className="w-11/12 shadow-md shadow-black rounded-xl p-4 m-4">
                 <p className="text-3xl font-bold text-emerald-700">CourseBlast</p>
-                <p className="text-xs italic ">Sign Up for CourseBlast</p>
-                    <div className="grid h-5/6">
-                        <form className="my-2">
+                <p className="text-lg">Sign Up for CourseBlast!</p>
+                    
+                    <div className="grid h-5/6 md:grid-cols-12">
+                        <form className="my-2 grid col-span-8 ">
                     <label htmlFor="name" className="block font-bold text-xl">Username</label>
-                    <input id="name" type='name' required placeholder="Username" className="outline-none p-1 md:w-8/12 rounded-md border-2 border-emerald-700 indent-2"/>
+                    <input id="name" type='name' required placeholder="Username" className="outline-none p-2 md:w-8/12 rounded-md border-2 border-emerald-700 indent-2 my-2"/>
 
-                    <label htmlFor="password" className="block font-bold">Password</label>
-                    <input id="password" type='password' required minLength={8} placeholder="Set a Password" className="p-1 peer md:w-8/12 rounded-md border-2 border-emerald-700 outline-none indent-2 my-1"/>
-                    <p className="peer-invalid:block hidden text-red-500">Password cannot be less than 8 characters</p>
+                    <label htmlFor="password" className="block font-bold text-xl">Password</label>
+                    <input id="password" type='password' required minLength={8} placeholder="Set a Password" className="p-2 peer md:w-8/12 rounded-md border-2 border-emerald-700 outline-none indent-2 my-2"/>
+                    <p className="peer-invalid:block hidden text-red-500 text-xs md:text-sm">Password cannot be less than 8 characters</p>
 
-                    <label htmlFor="email_add" className="block font-bold">Email</label>
-                    <input id="email_add" type='email' required placeholder="Email Address" className=" peer p-1 md:w-8/12 rounded-md border-2 border-emerald-700 outline-none indent-2 my-1"/>
-                    <p className="text-red-500 hidden peer-invalid:block">Please enter a valid e-mail address</p>
-                        </form>
+                    <label htmlFor="email_add" className="block font-bold text-xl">Email</label>
+                    <input id="email_add" type='email' required placeholder="Email Address" className=" peer p-2 md:w-8/12 rounded-md border-2 border-emerald-700 outline-none indent-2 my-2"/>
+                    <p className="text-red-500 hidden peer-invalid:block md:text-sm text-xs">
+                        Please enter a valid e-mail address
+                    </p>
 
-                        <button className="bg-emerald-700 w-10/12 md:w-6/12 p-2 rounded-3xl text-white justify-self-center self-end">
+                    <div className="my-2">
+                    <input type="checkbox" name="promotional" id="prom" className="p-2 checked:bg-emerald-700"/>
+                    <label htmlFor="prom" className="text-xs md:text-lg px-1">Receive promotional emails about course discounts, recommendations and early releases.</label>
+                    </div> 
+
+                    <p className="text-center my-2">OR</p>
+                    <div className="provider_bay grid justify-items-center">
+                    <div className="sign_in_provider shadow-sm shadow-emerald-700 p-2 my-2 rounded-xl border-emerald-700 
+                    border-2 md:w-10/12 hover:bg-emerald-700 hover:text-white">
+                        <div className="grid grid-cols-12 items-center">
+                            <FaGoogle fill="red" size={"2rem"} className="col-span-2 justify-self-center"/>
+                            <p className="col-span-6 text-center text-sm md:text-lg">Sign in with Google</p>
+                        </div>
+                    </div>
+                    <div className="sign_in_provider shadow-sm shadow-emerald-700 p-2 my-2 rounded-xl border-emerald-700 border-2 md:w-10/12 hover:bg-emerald-700 hover:text-white">
+                        <div className="grid grid-cols-12 items-center">
+                            <FaApple className="col-span-2 justify-self-center"size={'2rem'}/>
+                            <p className="col-span-6 text-center text-sm md:text-lg">Sign in with Apple</p>
+                        </div>
+                    </div>
+
+                    <div className="sign_in_provider shadow-sm shadow-emerald-700 p-2 my-2 rounded-xl border-emerald-700 border-2 md:w-10/12 hover:bg-emerald-700 hover:text-white">
+                        <div className="grid grid-cols-12 place-items-center">
+                            <FaFacebook fill="blue" size={'2rem'} className="col-span-2 justify-self-center"/>
+                            <p className="col-span-6 text-center text-sm md:text-lg">Sign in with Facebook</p>
+                        </div>
+                    </div>
+                    </div>
+                        <button className="bg-emerald-700 w-10/12 md:w-6/12 my-2 p-2 rounded-3xl text-white justify-self-center self-end">
                         Sign Up 
                     </button>
-
+                    </form>
                     </div>
+
+                    <p className="text-center w-full text-xs md:text-sm">By signing up for CourseBlast, you hereby agree to our Terms of Use and Privacy Policy</p>
+
                 </div>
         </div>
         </>
